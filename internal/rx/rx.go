17f0c8a16d7c0fdf1c887ae75b8a329180b14dec
Quo <quo@Kranon>
2023-09-01 08:03:35 +1000

    adding test receiving internal pkg

---
+++ internal/rx/rx.go
@@ -0,0 +1,46 @@
+package rx
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"mime/multipart"
+
+	"github.com/gabriel-vasile/mimetype"
+	"github.com/isuquo/templatemaker/internal/models"
+)
+
+func Test(t *models.Template, files []*multipart.FileHeader) (string, error) {
+
+	fmt.Printf("Template: %+v\n", t)
+	for _, f := range files {
+		file, err := f.Open()
+		if err != nil {
+			fmt.Printf("Error: %s\n", err.Error())
+			return "", err
+		}
+		defer file.Close()
+
+		mime, err := mimetype.DetectReader(file)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err.Error())
+			return "", err
+		}
+
+		fileType := mime.String()
+		file.Seek(0, io.SeekStart)
+
+		if fileType == "application/json" {
+			var jsonData interface{}
+			decoder := json.NewDecoder(file)
+			if err := decoder.Decode(&jsonData); err != nil {
+				fmt.Printf("Failed to decode JSON: %s\n", err.Error())
+				return "", err
+			}
+			fmt.Printf("JSON Content: %+v\n", jsonData)
+		}
+	}
+
+	return "", errors.New("AAAAAAAAAA")
+}
